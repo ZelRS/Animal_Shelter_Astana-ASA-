600066ab7210f060b6c24153a628aa5afef72906
Roman Zelenin (ZelRS) <ZeleninRS1991@gmail.com>
2023-11-25 18:09:09 +0600

    Покрыл комментариями класс MessageSender

--- src/main/java/pro/sky/telegramBot/sender/MessageSender.java
+++ src/main/java/pro/sky/telegramBot/sender/MessageSender.java
@@ -15,42 +15,51 @@ import java.util.concurrent.ConcurrentHashMap;
 
 import static com.pengrad.telegrambot.model.request.ParseMode.HTML;
 
+// данный класс предназначен для выполнения отпрвки (execute) сообщений определенного содержания пользователю
 @Service
 @RequiredArgsConstructor
 @Getter
 @Slf4j  // SLF4J logging
 public class MessageSender {
     private final TelegramBot telegramBot;
-
+    // мапа для текста, который не вошел в сообщение и который будет отправлен позже, если пользователь нажмет продолжить
+    // (пока не используется)
     private final Map<Long, String> remainingMessages = new ConcurrentHashMap<>();
 
-    public void sendHTMLMessage(long chatId, String messageText) {
-        log.info("Sending HTML text message to user");
-            SendMessage message = new SendMessage(String.valueOf(chatId), messageText).parseMode(HTML);
-            telegramBot.execute(message);
-    }
-
-    public void sendImageMessage(SendPhoto sendPhoto) {
-        log.info("Sending image message to user");
-            telegramBot.execute(sendPhoto);
-    }
-    public void sendImageMessageStart(SendPhoto sendPhoto) {
-        log.info("Sending start image message to user");
-        telegramBot.execute(sendPhoto);
-    }
-
+    // метод выполняет отравку простого текстового сообщения пользователю
     public void executeMessage(SendMessage message) {
         log.info("Message executing to user");
         telegramBot.execute(message);
     }
 
-    public void sendDocument(Long chatId, SendDocument sendDoc) {
-        log.info("Sending document message to user");
-        telegramBot.execute(sendDoc);
+    // метод выполняет отравку текстового сообщения пользователю с HTML разметкой
+    public void sendHTMLMessage(long chatId, String messageText) {
+        log.info("Sending HTML text message to user");
+        SendMessage message = new SendMessage(String.valueOf(chatId), messageText).parseMode(HTML);
+        telegramBot.execute(message);
     }
 
+    // метод выполняет отравку фото пользователю
+    public void sendImageMessage(SendPhoto sendPhoto) {
+        log.info("Sending image message to user");
+        telegramBot.execute(sendPhoto);
+    }
+
+    // метод выполняет отравку фото в стартовое сообщение пользователю
+    public void sendImageMessageStart(SendPhoto sendPhoto) {
+        log.info("Sending start image message to user");
+        telegramBot.execute(sendPhoto);
+    }
+
+    // метод выполняет отравку видео пользователю
     public void sendVideo(SendVideo sendVideoRequest) {
         log.info("Sending video message to user");
         telegramBot.execute(sendVideoRequest);
+    }
+
+    // метод выполняет отравку пользователю документа
+    public void sendDocument(Long chatId, SendDocument sendDoc) {
+        log.info("Sending document message to user");
+        telegramBot.execute(sendDoc);
     }
 }
