a99e80231bf9fb3101263e9d9460329c1c594fe2
Roman Zelenin (ZelRS) <ZeleninRS1991@gmail.com>
2023-11-25 18:52:30 +0600

    отредактировал классы создания и свщности кнопок, задав им более логичные названия. Покрыл код этих классов комментариями

--- src/main/java/pro/sky/telegramBot/handler/impl/MessageHandlerImpl.java
+++ src/main/java/pro/sky/telegramBot/handler/impl/MessageHandlerImpl.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Service;
 import pro.sky.telegramBot.config.BotConfig;
 import pro.sky.telegramBot.handler.MessageHandler;
 import pro.sky.telegramBot.sender.MessageSender;
-import pro.sky.telegramBot.utils.KeyboardCreator;
+import pro.sky.telegramBot.utils.CurrentKeyboardCreator;
 import pro.sky.telegramBot.utils.MediaMessageGenerator;
 
 @RequiredArgsConstructor
@@ -17,14 +17,14 @@ public class MessageHandlerImpl implements MessageHandler {
 
     private final MediaMessageGenerator mediaMessageGenerator;
     private final MessageSender messageSender;
-    private final KeyboardCreator keyboardCreator;
+    private final CurrentKeyboardCreator currentKeyboardCreator;
     private final BotConfig config;
     @Override
     public void sendWelcomeMessage(String firstName, Long chatId) {
         log.info("Sending welcome message to {}: {}", firstName, chatId);
         try {
             SendPhoto sendPhoto = mediaMessageGenerator.welcomeMessagePhotoCreate(chatId, firstName);
-            sendPhoto.replyMarkup(keyboardCreator.catsAndDogsMessageKeyboard());
+            sendPhoto.replyMarkup(currentKeyboardCreator.petSelectionMessageKeyboard());
             messageSender.sendImageMessage(sendPhoto);
         } catch (Exception e) {
             log.error("Failed to send welcome message to {}: {}", firstName, chatId, e);
--- src/main/java/pro/sky/telegramBot/utils/InlineKeyboardCreator.java
+++ src/main/java/pro/sky/telegramBot/utils/InlineKeyboardCreator.java
@@ -5,20 +5,22 @@ import com.pengrad.telegrambot.model.request.InlineKeyboardMarkup;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-import pro.sky.telegramBot.entity.Buttons;
+import pro.sky.telegramBot.entity.Button;
 
 
 import java.util.ArrayList;
 import java.util.List;
+
+// класс содержит мето для создания однострочной клавиатуры под сообщение, отправляемое пользователю
 @Service
 @RequiredArgsConstructor
 @Slf4j  // SLF4J logging
-public class ButtonsCreator {
+public class InlineKeyboardCreator {
 
-    public InlineKeyboardMarkup sendWithInlineKeyboard(List<Buttons> buttonsNames) {
+    public InlineKeyboardMarkup createInlineKeyboard(List<Button> buttonNames) {
 
         List<InlineKeyboardButton> buttons = new ArrayList<>();
-        for (Buttons buttonsName : buttonsNames) {
+        for (Button buttonsName : buttonNames) {
             String callbackData = buttonsName.getCallbackData();
             String buttonName = buttonsName.getName();
             InlineKeyboardButton button = new InlineKeyboardButton(buttonName).callbackData(callbackData);
