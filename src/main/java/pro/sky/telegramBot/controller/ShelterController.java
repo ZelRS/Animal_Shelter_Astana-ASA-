9791855545391f6afb35e994b5cbc5260cb022b6
Roman Zelenin (ZelRS) <ZeleninRS1991@gmail.com>
2023-11-26 11:36:50 +0600

    слегка доработал контроллеры и сервисы. внедрил с них базовые зависимости. подготовил к реализации эндпоинтов

--- src/main/java/pro/sky/telegramBot/controller/ShelterController.java
+++ src/main/java/pro/sky/telegramBot/controller/ShelterController.java
@@ -1,7 +1,12 @@
 package pro.sky.telegramBot.controller;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RestController;
+import pro.sky.telegramBot.service.ShelterService;
 
 @RestController
+@RequiredArgsConstructor
 public class ShelterController {
+
+    private final ShelterService shelterservice;
 }
