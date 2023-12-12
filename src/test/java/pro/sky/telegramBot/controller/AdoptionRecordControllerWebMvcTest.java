ca456d82a6e665d9d9422d1c95f16b1ec8f35160
YuriPetukhov <petuchowjuri@yahoo.de>
2023-12-12 16:43:34 +0300

    еще тесты для контроллера

--- src/test/java/pro/sky/telegramBot/controller/AdoptionRecordControllerWebMvcTest.java
+++ src/test/java/pro/sky/telegramBot/controller/AdoptionRecordControllerWebMvcTest.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.mock.mockito.SpyBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import pro.sky.telegramBot.config.BotConfig;
 import pro.sky.telegramBot.enums.UserState;
@@ -26,6 +27,7 @@ import pro.sky.telegramBot.utils.statistic.ReportDataConverter;
 import pro.sky.telegramBot.utils.statistic.ReportSumCalculator;
 import pro.sky.telegramBot.utils.statistic.StatisticPreparer;
 
+import java.util.NoSuchElementException;
 import java.util.Optional;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
@@ -36,6 +38,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static pro.sky.telegramBot.enums.TrialPeriodState.*;
+import static pro.sky.telegramBot.enums.UserState.BLOCKED;
 
 @WebMvcTest(AdoptionRecordController.class)
 class AdoptionRecordControllerWebMvcTest {
@@ -165,6 +168,92 @@ class AdoptionRecordControllerWebMvcTest {
 
         // Проверяем, что статус исходной записи был изменен на CLOSED
         assertThat(originalAdoptionRecord.getState()).isEqualTo(CLOSED);
+    }
+    @Test
+    void testExtendAdoptionRecord_Failure() throws Exception {
+        // Подготовка данных для теста
+        Long adoptionRecordId = 1L;
+
+        // Исходная запись об усыновлении с неправильным статусом для продления
+        AdoptionRecord originalAdoptionRecord = new AdoptionRecord();
+        originalAdoptionRecord.setId(adoptionRecordId);
+        originalAdoptionRecord.setState(UNSUCCESSFUL);
+
+        // Мокирование вызовов методов
+        // Имитация отсутствия записи, которую можно было бы продлить
+        when(adoptionRecordRepository.findById(adoptionRecordId)).thenReturn(Optional.of(originalAdoptionRecord));
+
+        // Вызываем метод контроллера и ожидаем ошибку
+        mockMvc.perform(put("/adoption/record/{id}", adoptionRecordId)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isCreated());
+
+        // Проверяем, что метод findById был вызван
+        verify(adoptionRecordRepository).findById(adoptionRecordId);
+
+        // Проверяем, что метод extendAdoptionRecord был вызван
+        verify(adoptionRecordService).extendAdoptionRecord(adoptionRecordId);
+
+        // Убедимся, что другие методы не вызывались, так как должно было произойти исключение
+        verify(adoptionRecordRepository, never()).save(any(AdoptionRecord.class));
+        verify(userService, never()).update(any(User.class));
+        verify(petService, never()).update(any(Pet.class));
+    }
+    @Test
+    void testTerminateAdoptionRecord_Success() throws Exception {
+        // Подготовка данных для теста
+        Long adoptionRecordId = 1L;
+        AdoptionRecord adoptionRecord = new AdoptionRecord();
+        User user = new User();
+        Pet pet = new Pet();
+
+        adoptionRecord.setId(adoptionRecordId);
+        adoptionRecord.setUser(user);
+        adoptionRecord.setPet(pet);
+        // Здесь должен быть код настройки начального состояния AdoptionRecord, User и Pet
+
+        // Мокирование вызовов методов
+        when(adoptionRecordRepository.findById(adoptionRecordId)).thenReturn(Optional.of(adoptionRecord));
+        when(adoptionRecordRepository.save(any(AdoptionRecord.class))).thenReturn(adoptionRecord);
+        when(userService.update(any(User.class))).thenReturn(user);
+        when(petService.update(any(Pet.class))).thenReturn(pet);
+
+        // Вызываем метод контроллера
+        mockMvc.perform(put("/adoption/record/terminate/{id}", adoptionRecordId)
+                        .param("adoptionRecordId", adoptionRecordId.toString()))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(adoptionRecordId));
+
+        // Проверка, что все моки были вызваны с ожидаемыми параметрами
+        verify(adoptionRecordRepository).findById(adoptionRecordId);
+        verify(adoptionRecordRepository).save(any(AdoptionRecord.class)); // Проверяем, был ли сохранен
+        verify(userService).update(any(User.class));
+        verify(petService).update(any(Pet.class));
+
+        // Проверяем изменения в объектах
+        assertThat(adoptionRecord.getState()).isEqualTo(CLOSED);
+        assertThat(user.getState()).isEqualTo(BLOCKED);
+        assertThat(user.getPet()).isNull();
+        assertThat(pet.getOwner()).isNull();
+    }
+
+    @Test
+    void testTerminateAdoptionRecord_NotFound() throws Exception {
+        // Подготовка данных для теста
+        Long adoptionRecordId = 1L;
+
+        // Мокирование вызовов методов для сценария "запись не найдена"
+        when(adoptionRecordRepository.findById(adoptionRecordId)).thenReturn(Optional.empty());
+
+        // Вызываем метод контроллера и ожидаем статус 404
+        mockMvc.perform(put("/adoption/record/terminate/{id}", adoptionRecordId))
+                .andExpect(status().isNotFound());
+
+        // Проверка, что метод findById был вызван и проверяется отсутствие дальнейшего взаимодействия
+        verify(adoptionRecordRepository).findById(adoptionRecordId);
+        verify(adoptionRecordRepository, never()).save(any(AdoptionRecord.class));
+        verify(userService, never()).update(any(User.class));
+        verify(petService, never()).update(any(Pet.class));
     }
 
 }
